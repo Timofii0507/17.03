3ec71407ea40cdb57e084067e28bbea26832d63d
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-20 14:17:03 +0200

    Strategy

---
+++ ConcreteStrategyB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._03
+{
+    public class ConcreteStrategyB : IStrategy
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Виконання алгоритму B");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            var context = new Context(new ConcreteStrategyA());
+            context.ExecuteStrategy();
+
+            context.SetStrategy(new ConcreteStrategyB());
+            context.ExecuteStrategy(); 
+        }
+    }
+}
