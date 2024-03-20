21df157ee9ecbf3ad241cc315f19bbdd9eca83c9
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-20 14:26:53 +0200

    Template Method

---
+++ ConcreteClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._03
+{
+    public class ConcreteClass : AbstractClass
+    {
+        protected override void RequiredOperations1()
+        {
+            Console.WriteLine("ConcreteClass says: Implemented Operation1");
+        }
+
+        protected override void RequiredOperation2()
+        {
+            Console.WriteLine("ConcreteClass says: Implemented Operation2");
+        }
+
+        protected override void Hook1()
+        {
+            Console.WriteLine("ConcreteClass says: Overridden Hook1");
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            Console.WriteLine("Same client code can work with different subclasses:");
+            AbstractClass abstractClass = new ConcreteClass();
+            abstractClass.TemplateMethod();
+        }
+    }
+}
